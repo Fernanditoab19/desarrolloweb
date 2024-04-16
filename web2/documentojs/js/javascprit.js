8b2b13e7cee3903be58a6fc429fb360d38f3c786
Fernanditoab19 <luisfer.aponte191998@gmail.com>
2024-04-16 08:31:21 -0400

    commit 2

---
+++ web2/documentojs/js/javascprit.js
@@ -0,0 +1,10 @@
+/*
+Desarrollador: Fernando Aponte
+ Fecha: 2024-04-15
+ Cambios: Ninguno
+
+ Descripción: Este es un ejemplo básico en JavaScript que muestra "Hola mundo" en la consola.
+*/
+
+var mensaje="imprimir en consola";
+console.log(mensaje)
---
+++ web2/documentojs/js/javascprit01.js
@@ -0,0 +1,13 @@
+/*
+Descripción: Ejemplo de variable e impresión en consola.
+
+Desarrollador:Nombre y apellido
+Fecha: 2024-04-15
+Cambios :Ninguno
+*/
+
+var color= prompt ("Ingrese un COLOR");
+console. log("Color ingresa es ",color);
+var num1= prompt ("Ingrese un NUMERO");
+var num2= prompt ("Ingrese OTRO NUMERO");
+console. log("La suma de", num1, "+",num2,"=", num1+num2)
